e46bfce807fccab214f01a8c83e2e802f0f699e8
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-06-20 09:40:01 +0200

    Styling the sidebar & button with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProjectSidebar from "@components/ProjectSidebar/ProjectSidebar.jsx";
 
 function App() {
   return (
-    <main>
+    <main className="flex flex-col md:flex-row h-screen my-8">
       <ProjectSidebar />
     </main>
   );
